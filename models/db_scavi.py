18c3398a2ff7157159e5ba5a41a636ce1c8a83e8
Piotr Bogun <pbogun1123@gmail.com>
2015-03-18 17:53:11 -0500

    Final styling, completed REST calls, added functionality to some views, Matt Kula's programming is included in this commit

--- models/db_scavi.py
+++ models/db_scavi.py
@@ -12,7 +12,7 @@ db.define_table(
     Field('hunt_id', 'reference scavenger_hunt'),
     Field('question', 'text', requires = IS_NOT_EMPTY()),
     Field('answer', 'text', requires = IS_NOT_EMPTY()),
-    Field('points', 'integer', requires = IS_NOT_EMPTY()),
+    Field('points', 'integer', requires = IS_NOT_EMPTY(), default = 5),
     Field('latitude', 'double', requires = IS_NOT_EMPTY()),
     Field('longitude', 'double', requires = IS_NOT_EMPTY()),
     Field('clue_number', 'integer', requires = IS_NOT_EMPTY()))
@@ -21,11 +21,12 @@ db.clue.hunt_id.requires = IS_IN_DB(db, db.scavenger_hunt.id, '%(name)s')
 
 db.define_table(
     'scavi_session',
-    Field('user_id', 'reference auth_user'),
+    Field('user_id', 'reference auth_user', requires = IS_NOT_EMPTY()),
     Field('hunt_id', 'reference scavenger_hunt'),
     Field('next_clue_id', 'reference clue'),
-    Field('points', 'integer'),
-    Field('is_in_progress', 'boolean', default=False))
+    Field('current_clue_number', 'integer', default = 0),
+    Field('points', 'integer', default = 0),
+    Field('is_in_progress', 'boolean', default=True))
 
 db.scavi_session.user_id.requires = IS_IN_DB(db, db.auth_user.id,
                                     '%(last_name)s, %(first_name)s')
